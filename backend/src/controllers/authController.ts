a0b6b41c9f316ca27323847eaab33970d232d12e
Sandesh <bhandarisandesh64@gmail.com>
2024-11-23 21:17:13 +0545

    Added login functionality

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import NewUserModel from "../models/registerModel";
+import { comparePasswords } from "../utils/bcrypt";
 
 export const Register: RequestHandler = async (req, res): Promise<any> => {
   try {
@@ -20,6 +21,30 @@ export const Register: RequestHandler = async (req, res): Promise<any> => {
       return res
         .status(201)
         .json({ message: "User created successfully", newUser });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error!" });
+  }
+};
+
+export const Login: RequestHandler = async (req, res): Promise<any> => {
+  try {
+    const { password, username } = req.body;
+
+    const user = await NewUserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const passwordsMatch = await comparePasswords(password, user.password);
+
+    if (passwordsMatch) {
+      return res.status(200).json({ message: "Login successful" });
+    } else {
+      return res
+        .status(401)
+        .json({ message: "Incorrect username or password!" });
     }
   } catch (error) {
     return res.status(500).json({ message: "Internal server error!" });
--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { Register } from "../controllers/authController";
+import { Login, Register } from "../controllers/authController";
 
 const authRouter = express.Router();
 
 authRouter.route("/register").post(Register);
+authRouter.route("/login").post(Login);
 
 export default authRouter;
